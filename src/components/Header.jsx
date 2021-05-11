727d6ee3fcf6c97e25ffd94ede6fc3b4f50423c6
Abhay Gupta <abhaygupta15153986@gmail.com>
2021-05-11 22:29:43 +0530

    Keeper Complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CreateIcon from "@material-ui/icons/Create";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CreateIcon /> KEEPER
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
